b5fc609e1c0b0f81ce6d003400d0e8569cb98ca5
Dmitry Vakhnin <jsbmand@gmail.com>
2024-02-06 17:54:19 -0300

    Fixed bug when location int parsing fails

--- lib/src/entities/location.dart
+++ lib/src/entities/location.dart
@@ -3,11 +3,14 @@ class GoogleGeocodingLocation {
   const GoogleGeocodingLocation({required this.lat, required this.lng});
 
   /// [GoogleGeocodingLocation] From Json factory
-  factory GoogleGeocodingLocation.fromJson(Map<String, dynamic> json) =>
-      GoogleGeocodingLocation(
-        lat: json['lat'] as double,
-        lng: json['lng'] as double,
-      );
+  factory GoogleGeocodingLocation.fromJson(Map<String, dynamic> json) {
+    final lat = json['lat'];
+    final lng = json['lng'];
+    return GoogleGeocodingLocation(
+      lat: lat is int ? lat.toDouble() : lat as double,
+      lng: lng is int ? lng.toDouble() : lng as double,
+    );
+  }
 
   /// Latitude
   final double lat;
